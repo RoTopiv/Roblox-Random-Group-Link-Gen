75dc211138434ecd7f1a2f1bab70e264c9949751
RoTopiv <111011932+RoTopiv@users.noreply.github.com>
2022-08-10 11:56:17 -0700

    Add files via upload

---
+++ Roblox Group Links.py
@@ -0,0 +1,17 @@
+from multiprocessing.connection import wait
+from multiprocessing.sharedctypes import Value
+from ntpath import join
+from os import link
+import string
+from tkinter import S
+import webbrowser
+import random
+import urllib.request
+
+number = str(random.randint(1000, 100000))
+
+str1 = 'https://www.roblox.com/groups/' + number
+
+print(str1)
+
+webbrowser.open_new_tab(str1)
